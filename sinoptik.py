935f41dc9a0f5d703e5b78c893c45689eae30a0e
la2xaoc <51865678+la2xaoc@users.noreply.github.com>
2019-07-10 02:44:50 +0300

    Update sinoptik.py

--- sinoptik.py
+++ sinoptik.py
@@ -99,12 +99,12 @@ def send_text(message):
 
     elif message.text == "Погода на завтра":
         now = datetime.now()
-        now = now + timedelta(hours=4)
+        now = now + timedelta(hours=3)
         
         one_days = timedelta(1) # плюсует следущий день
         in_two_days = now + one_days
-        datapogoda = in_two_days.strftime("%Y-%m-%d %I:%M") # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
-        bot.send_message(message.chat.id, datapogoda)
+        datapogoda = in_two_days.strftime("%Y-%m-%d") # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
+        #bot.send_message(message.chat.id, datapogoda)
         s1 = requests.get('https://sinoptik.com.ru/погода-луцк/' + datapogoda)  # следущий день
 
         b = bs4.BeautifulSoup(s1.text, "html.parser")
