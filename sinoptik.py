73e98dd5f27549a60d20d79acaf455df8b04938a
la2xaoc <51865678+la2xaoc@users.noreply.github.com>
2019-07-18 07:47:22 +0300

    Update sinoptik.py

--- sinoptik.py
+++ sinoptik.py
@@ -20,8 +20,8 @@ def send_welcom(message):
 @bot.message_handler(func=lambda message: True)
 def send_text(message):
     if message.text == "Погода на сегодня":
-        bot.send_message(message.chat.id, lutsk.answer)
         bot.send_message(message.chat.id, lutsk.datapogoda)
+        bot.send_message(message.chat.id, lutsk.answer)
 
     elif message.text == "Погода на завтра":
         bot.send_message(message.chat.id, lutsk.answer2)
