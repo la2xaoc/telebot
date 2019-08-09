5f4f81bb11df55b37cb36594ed5fbebba3b38ba1
la2xaoc <51865678+la2xaoc@users.noreply.github.com>
2019-08-10 01:48:35 +0300

    Update sinoptik.py

--- sinoptik.py
+++ sinoptik.py
@@ -5,22 +5,22 @@ from datetime import datetime, timedelta
 
 bot = telebot.TeleBot("842277315:AAGYaZV9kRdrvGUdpVLmONUaT-qUYyJvB5o")
 
-markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-btn_pogoda1 = types.KeyboardButton('Погода на сегодня')
-btn_pogoda2 = types.KeyboardButton('Погода на завтра')
-markup_menu.add(btn_pogoda1, btn_pogoda2)
+markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)  #выбор города
+btn_pogoda1 = types.KeyboardButton('help')  #выбор города
+markup_menu.add(btn_pogoda1)  #выбор города
 
 
-@bot.message_handler(commands=['start'])
-def send_welcom(message):
-    bot.send_message(message.chat.id, "Привет, я бот прогноза погоды", reply_markup=markup_menu)
+def keyboard():
+    markup_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn_pogoda1 = types.KeyboardButton('Погода на сегодня')
+    btn_pogoda2 = types.KeyboardButton('Погода на завтра')
+    markup_menu.add(btn_pogoda1, btn_pogoda2)
+    return markup_menu
 
 
-@bot.message_handler(func=lambda message: True)
-def send_text(message):
-    if message.text == "Погода на сегодня":
-        s = requests.get('https://sinoptik.com.ru/погода-луцк')
-
+def today():
+    s = requests.get('https://sinoptik.com.ru/погода-' + gorod)
+    if s:
         b = bs4.BeautifulSoup(s.text, "html.parser")
 
         p3 = b.select('#bd1c .temperature .p3')
@@ -56,7 +56,20 @@ def send_text(message):
         daylink = b.select('#bd1 .day-link')
         daylink1 = daylink[0].getText()
 
-        answer = daylink1 + ' ' + date1 + ' ' + month1 + "\n\n"
+        now = datetime.now()
+        now = now + timedelta(hours=3)
+
+        one_days = timedelta(0)  # плюсует следущий день
+        in_two_days = now + one_days
+        datapogoda = in_two_days.strftime(
+            "%Y-%m-%d")  # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
+        # bot.send_message(message.chat.id, datapogoda)
+        dataminuts = in_two_days.strftime(
+            "%Y-%m-%d %H:%M:%S")
+
+        answer = dataminuts + "\n\n"
+
+        answer += daylink1 + ' ' + date1 + ' ' + month1 + "\n\n"
 
         answer += 'Утром :' + pogoda1 + ' ' + pogoda2 + "\n"
 
@@ -94,32 +107,116 @@ def send_text(message):
         else:
             answer += ""
 
-        now = datetime.now()
-        now = now + timedelta(hours=3)
+        return answer
+        # bot.send_message(call.message.chat.id, dataminuts)
+        # bot.send_message(call.message.chat.id, answer)
+    else:
+        s = requests.get('https://sinoptik.com.ru/погода-' + gorod)
+        if s:
+            b = bs4.BeautifulSoup(s.text, "html.parser")
 
-        one_days = timedelta(0)  # плюсует следущий день
-        in_two_days = now + one_days
-        datapogoda = in_two_days.strftime(
-            "%Y-%m-%d")  # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
-        # bot.send_message(message.chat.id, datapogoda)
-        dataminuts = in_two_days.strftime(
-            "%Y-%m-%d %H:%M:%S")
-        bot.send_message(message.chat.id, dataminuts)
-        bot.send_message(message.chat.id, answer)
+            p3 = b.select('#bd1c .temperature .p3')
 
-    elif message.text == "Погода на завтра":
-        now = datetime.now()
-        now = now + timedelta(hours=3)
+            pogoda1 = p3[0].getText()
 
-        one_days = timedelta(1)  # плюсует следущий день
-        in_two_days = now + one_days
-        datapogoda = in_two_days.strftime(
-            "%Y-%m-%d")  # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
-        # bot.send_message(message.chat.id, datapogoda)
-        dataminuts = in_two_days.strftime(
-            "%Y-%m-%d %H:%M:%S")
-        s1 = requests.get('https://sinoptik.com.ru/погода-луцк/' + datapogoda)  # следущий день
+            p4 = b.select('.temperature .p4')
 
+            pogoda2 = p4[0].getText()
+
+            p5 = b.select('.temperature .p5')
+
+            pogoda3 = p5[0].getText()
+
+            p6 = b.select('.temperature .p6')
+
+            pogoda4 = p6[0].getText()
+
+            p7 = b.select('.temperature .p7')
+
+            pogoda5 = p7[0].getText()
+
+            p8 = b.select('.temperature .p8')
+
+            pogoda6 = p8[0].getText()
+
+            date = b.select('#bd1 .date')
+            date1 = date[0].getText()
+
+            month = b.select('#bd1 .month')
+            month1 = month[0].getText()
+
+            daylink = b.select('#bd1 .day-link')
+            daylink1 = daylink[0].getText()
+
+            now = datetime.now()
+            now = now + timedelta(hours=3)
+
+            one_days = timedelta(0)  # плюсует следущий день
+            in_two_days = now + one_days
+            datapogoda = in_two_days.strftime(
+                "%Y-%m-%d")  # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
+            # bot.send_message(message.chat.id, datapogoda)
+            dataminuts = in_two_days.strftime(
+                "%Y-%m-%d %H:%M:%S")
+
+            answer = dataminuts + "\n\n"
+
+            answer += daylink1 + ' ' + date1 + ' ' + month1 + "\n\n"
+
+            answer += 'Утром :' + pogoda1 + ' ' + pogoda2 + "\n"
+
+            answer += 'Днём :' + pogoda3 + ' ' + pogoda4 + "\n"
+
+            answer += 'Вечер :' + pogoda5 + ' ' + pogoda6 + "\n\n"
+
+            pico1 = b.select('.rSide .ico-stormWarning-1')  # предупреждение град, гроза, пожарность
+            pico3 = b.select('.rSide .ico-stormWarning-3')  # ветер
+            pw = b.select('.wDescription')  # прогноз погоды описание
+            p = b.select('.oDescription .rSide .description')  # Народный прогноз погоды
+            answer += "Тут может быть ваша реклама. Контакт: @Arganaft\n\n"
+
+            if (pico1):  # предупреждение град, гроза, пожарность
+                pogoda = pico1[0].getText()
+                answer += pogoda.strip() + "\n\n"
+            else:
+                answer += ""
+
+            if (pico3):  # ветер
+                pogoda = pico3[0].getText()
+                answer += pogoda.strip() + "\n\n"
+            else:
+                answer += ""
+
+            if (pw):  # прогноз погоды описание
+                pogoda = pw[0].getText()
+                answer += pogoda.strip() + "\n\n"
+            else:
+                answer += ""
+
+            if (p):  # Народный прогноз погоды
+                pogoda = p[0].getText()
+                answer += pogoda.strip()
+            else:
+                answer += ""
+
+            return answer
+            # bot.send_message(call.message.chat.id, dataminuts)
+            # bot.send_message(call.message.chat.id, answer)
+
+
+def tomorrow():
+    now = datetime.now()
+    now = now + timedelta(hours=3)
+
+    one_days = timedelta(1)  # плюсует следущий день
+    in_two_days = now + one_days
+    datapogoda = in_two_days.strftime(
+        "%Y-%m-%d")  # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
+    # bot.send_message(message.chat.id, datapogoda)
+    dataminuts = in_two_days.strftime(
+        "%Y-%m-%d %H:%M:%S")
+    s1 = requests.get('https://sinoptik.com.ru/погода-' + gorod + '/' + datapogoda)  # следущий день
+    if s1:
         b = bs4.BeautifulSoup(s1.text, "html.parser")
 
         p3 = b.select('.temperature .p3')
@@ -168,10 +265,111 @@ def send_text(message):
         pogoda = p[0].getText()
 
         answer2 += pogoda.strip()
-        bot.send_message(message.chat.id, answer2)
+        return answer2
+        #bot.send_message(message.chat.id, answer2)
+    else:
+        now = datetime.now()
+        now = now + timedelta(hours=3)
 
+        one_days = timedelta(1)  # плюсует следущий день
+        in_two_days = now + one_days
+        datapogoda = in_two_days.strftime(
+            "%Y-%m-%d")  # .strftime("%Y-%m-%d") задаает формат даты в сроку и дает ноль спереди
+        # bot.send_message(message.chat.id, datapogoda)
+        dataminuts = in_two_days.strftime(
+            "%Y-%m-%d %H:%M:%S")
+        s1 = requests.get('https://sinoptik.com.ru/погода-' + gorod + '/' + datapogoda)  # следущий день
+        if s1:
+            b = bs4.BeautifulSoup(s1.text, "html.parser")
+
+            p3 = b.select('.temperature .p3')
+
+            pogoda3 = p3[0].getText()
+
+            p4 = b.select('.temperature .p4')
+
+            pogoda4 = p4[0].getText()
+
+            p5 = b.select('.temperature .p5')
+
+            pogoda5 = p5[0].getText()
+
+            p6 = b.select('.temperature .p6')
+
+            pogoda6 = p6[0].getText()
+
+            p7 = b.select('.temperature .p7')
+
+            pogoda7 = p7[0].getText()
+
+            p8 = b.select('.temperature .p8')
+
+            pogoda8 = p8[0].getText()
+
+            date = b.select('#bd2 .date')
+            date1 = date[0].getText()
+
+            month = b.select('#bd2 .month')
+            month1 = month[0].getText()
+
+            daylink = b.select('#bd2 .day-link')
+            daylink1 = daylink[0].getText()
+
+            answer2 = daylink1 + ' ' + date1 + ' ' + month1 + "\n\n"
+
+            answer2 += 'Утром :' + pogoda3 + ' ' + pogoda4 + "\n"
+
+            answer2 += 'Днём :' + pogoda5 + ' ' + pogoda6 + "\n"
+
+            answer2 += 'Вечер :' + pogoda7 + ' ' + pogoda8 + "\n\n"
+
+            p = b.select('.rSide .description')
+
+            pogoda = p[0].getText()
+
+            answer2 += pogoda.strip()
+            return answer2
+            # bot.send_message(message.chat.id, answer2)
+
+
+markup_tomorrow = types.InlineKeyboardMarkup(row_width=2)  #инлайн кнопки
+button_1 = types.InlineKeyboardButton(text='Погода на сегодня', callback_data='today')
+button_2 = types.InlineKeyboardButton(text='Погода на завтра', callback_data='tomorrow')
+markup_tomorrow.add(button_1, button_2)
+
+
+gorod = ''
+
+help1 = "Привет, я бот прогноза погоды!\n\nВводить можно русскими и украинскими буквами. Пример: \n Киев или киев"
+
+
+@bot.message_handler(commands=['start'])
+def send_welcom(message):
+    bot.send_message(message.chat.id, help1, reply_markup=markup_menu)
+
+
+@bot.callback_query_handler(func=lambda c: True)
+def inlin(c):
+    if c.message:
+        if c.data == "today":
+            bot.send_message(c.message.chat.id, today())
+        elif c.data == "tomorrow":
+            bot.send_message(c.message.chat.id, tomorrow())
+        else:
+            bot.send_message(c.message.chat.id, 'Введите снова')
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def send_text(message):
+    global gorod
+    gorod = message.text
+    if gorod != 'help':
+        bot.send_message(message.chat.id, gorod, reply_markup=markup_tomorrow)
+    elif message.text == 'help':
+        gorod = 'киев'
+        bot.send_message(message.chat.id, 'Вводить можно русскими и украинскими буквами. Пример:')
+        bot.send_message(message.chat.id, 'киев', reply_markup=markup_tomorrow)
+    else:
+        bot.send_message(message.chat.id, 'Ошибка! Повторите...')
 
 bot.polling(none_stop=True)
-
-
-
